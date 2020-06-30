06f2752a1678bbb5f2b35d35b62a9d124042dfa3
Ramya Bhaskara <ramyasb@google.com>
2020-06-30 18:36:39 +0000

    Create dropdown with hard-coded courses

---
+++ src/main/java/com/google/servlets/CourseListServlet.java
@@ -0,0 +1,54 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+
+import java.io.File; 
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.Scanner;
+import org.json.simple.JSONObject;
+
+
+/** Servlet that returns some example content.*/
+@WebServlet("/courselist")
+public class CourseListServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException { 
+    // TODO: Store and retrieve from Datastore
+    String [] courseList = new String[]{"CMSC101", "CMSC106", "CMSC122", "CMSC131",
+        "CMSC132","CMSC133","CMSC216","CMSC250","CMSC298A","CMSC320","CMSC330","CMSC351",
+        "CMSC388J","CMSC389A","CMSC389B","CMSC389E","CMSC389N","CMSC390O"};
+    
+    response.setContentType("text/html;");
+    response.getWriter().println(convertToJson(courseList));
+  }
+
+  /**
+   * Converts a ServerStats instance into a JSON string using the Gson library. Note: We first added
+   * the Gson library dependency to pom.xml.
+   */
+  private String convertToJson(String [] courses) {
+    Gson gson = new Gson();
+    String json = gson.toJson(courses);
+    return json;
+  }
+}
---
+++ src/main/webapp/scripts.js
@@ -0,0 +1,43 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * Gets courses from /courselist servlet to populate dropdown list
+ */
+async function getOptions() {
+  const response = await fetch('/courselist');
+  const courses = await response.json();
+  const courseContainer = document.getElementById('courses');
+  courseContainer.innerHTML = '';
+  createOption('Select a Course', 'courses', /*setValue=*/ false);
+  courses.forEach(course => createOption(course, 'courses', /*setValue=*/ true));
+}
+/**
+ * Creates options in select list
+ * @param {string} course The name of the course to add to the dropdown
+ * @param {string} container The id name of the container you want to add options to
+ * @param {boolean} setValue Whether to set the value of the option to the string 
+ */
+function createOption(course, container, setValue) {
+    const courseContainer = document.getElementById(container);
+    const opt = document.createElement('option');
+    opt.innerText = course;
+    if (setValue) {
+      opt.value = course;
+    } else {
+        opt.selected = true;
+        opt.hidden = true;
+    }
+    courseContainer.appendChild(opt);
+}
