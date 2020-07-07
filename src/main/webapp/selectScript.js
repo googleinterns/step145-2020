0db213330f94aea14531e375eb6805e8fa785c03
Ramya Bhaskara <ramyasb@google.com>
2020-07-07 19:35:09 +0000

    Separate course selection and calendar scripts

--- src/main/webapp/selectScript.js
+++ src/main/webapp/selectScript.js
@@ -99,22 +99,4 @@ window.addEventListener('load', () => {
   }
 
   getOptions();
-
-  function initCalendar() {
-    new tui.Calendar('#calendar', {
-      defaultView: 'week',
-      useCreationPopup: true,
-      useDetailPopup: true,
-      disableDblClick: true,
-      disableClick: true,
-      isReadOnly: true,
-      scheduleView: ['time'],
-      taskView: false,
-      week: {
-        workweek: true,
-      },
-    });
-  }
-
-  initCalendar();
 });
