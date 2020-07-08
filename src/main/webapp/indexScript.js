00623f27137dda39bf44bd5ad96ef0e84b585f37
Ramya Bhaskara <ramyasb@google.com>
2020-07-08 15:59:21 +0000

    Add better separation of scripts

--- src/main/webapp/indexScript.js
+++ src/main/webapp/indexScript.js
@@ -99,4 +99,22 @@ window.addEventListener('load', () => {
   }
 
   getOptions();
+
+  function initCalendar() {
+    new tui.Calendar('#calendar', {
+      defaultView: 'week',
+      useCreationPopup: true,
+      useDetailPopup: true,
+      disableDblClick: true,
+      disableClick: true,
+      isReadOnly: true,
+      scheduleView: ['time'],
+      taskView: false,
+      week: {
+        workweek: true,
+      },
+    });
+  }
+
+  initCalendar();
 });
