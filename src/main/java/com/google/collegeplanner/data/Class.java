6a081961e38b3768d577c8f6381f641e8fabd6e6
naaoli <naaoli.mamie8@gmail.com>
2020-07-13 19:10:56 +0000

    Add Class class and tests

---
+++ src/main/java/com/google/collegeplanner/data/Class.java
@@ -0,0 +1,107 @@
+package com.google.collegeplanner.data;
+
+public class Class {
+  private String courseId;
+  private String name;
+  private String semester;
+  private int credits;
+  private String deptId;
+  private String description;
+  private String[] gradingMethod;
+  private String coreqs;
+  private String prereqs;
+  private String restrictions;
+  private String additionalInfo;
+  private String creditGrantedFor;
+
+  public Class(String courseId, String name, String semester, int credits, String deptId, String description, 
+  String[] gradingMethod, String coreqs, String prereqs, String restrictions, String additionalInfo, String creditGrantedFor) {
+    this.courseId = courseId;
+    this.name = name;
+    this.semester = semester;
+    this.credits = credits;
+    this.deptId = deptId;
+    this.description = description;
+    this.gradingMethod = gradingMethod;
+    this.coreqs = coreqs;
+    this.prereqs = prereqs;
+    this.restrictions = restrictions;
+    this.additionalInfo = additionalInfo;
+    this.creditGrantedFor = creditGrantedFor; 
+  }
+
+  // Returns the season of the semester
+  // The format of the semester String is the four digit year followed by
+  // the two digit start month.
+  public String getSemesterSeason() {
+    // startMonth stores the last two digits of the semester String as an int
+    int startMonth = Integer.parseInt(semester) % 100;
+
+    if(startMonth == 1 || startMonth == 2) {
+      return "Spring";
+    } else if(startMonth == 8 || startMonth == 9) {
+      return "Fall";
+    } else if(startMonth == 6) {
+      return "Summer I";
+    } else if(startMonth == 7) {
+      return "Summer II";
+    }
+    
+    return "Winter";
+  }
+
+  // Returns the year of the semester, which is the first four digits of the
+  // semester String as an int
+  public int getSemesterYear() {
+    return Integer.parseInt(semester.substring(0,4));
+  }
+
+  // Getter Methods
+  public String getCourseId() {
+    return courseId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getSemester() {
+    return semester;
+  }
+
+  public int getCredits() {
+    return credits;
+  }
+
+  public String getDeptId() {
+    return deptId;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String[] getGradingMethod() {
+    return gradingMethod;
+  }
+
+  public String getCoreqs() {
+    return coreqs;
+  }
+
+  public String getPrereqs() {
+    return prereqs;
+  }
+
+  public String getRestrictions() {
+    return restrictions;
+  }
+
+  public String getAdditionalInfo() {
+    return additionalInfo;
+  }
+
+  public String getCreditGrantedFor() {
+    return creditGrantedFor;
+  }
+}
