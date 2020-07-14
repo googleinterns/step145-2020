70ed4356454f5799a4ccf38c5b1875b1ab1a5b7d
savsa <savs@google.com>
2020-07-14 23:51:56 +0000

    Fix formatting issues.

--- src/test/java/com/google/collegeplanner/DepartmentTest.java
+++ src/test/java/com/google/collegeplanner/DepartmentTest.java
@@ -14,8 +14,8 @@
 package com.google.collegeplanner;
 
 import static org.mockito.AdditionalMatchers.not;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -95,7 +95,8 @@ public final class DepartmentTest {
     servlet.doGet(null, mockedResponse);
     // Verifies whether status was set to SC_INTERNAL_SERVER_ERROR
     verify(mockedResponse, times(1)).setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-    verify(mockedResponse, never()).setStatus(not(eq(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)));
+    verify(mockedResponse, never())
+        .setStatus(not(eq(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)));
     JSONParser parser = new JSONParser();
     JSONObject responseJson = (JSONObject) parser.parse(stringWriter.toString());
     String expectedJson = "{\"message\":\"Internal server error.\",\"status\":\"error\"}";
