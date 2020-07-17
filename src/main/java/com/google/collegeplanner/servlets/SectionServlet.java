f443537bcd250c1227c5ad124bad5ede48a509bc
Savion <savs@google.com>
2020-07-16 17:13:04 -0700

    Add Calendar Events (#29)
    * Rebase to master.
    
    * Fix formatting issues.
    
    * Change SectionServlet constructor.
    
    * Rename function and update TODO comments.
    
    * Change time zone of calendar.
    
    * Use the Luxon library to handle time.
    
    * Add minor change.
    
    * Move most things out of the calendar export.
    
    * Change to full-week calendar.
    
    * Fix timezone offset and add comment about it.
    
    * Fix comments.
    
    * Update comments to fit jsdoc style.

---
+++ src/main/java/com/google/collegeplanner/servlets/SectionServlet.java
@@ -0,0 +1,83 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.collegeplanner.servlets;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+/** Servlet that returns list of course sections.*/
+@WebServlet("/api/sections")
+public class SectionServlet extends HttpServlet {
+  ApiUtil apiUtil;
+
+  public SectionServlet() {
+    this(new ApiUtil());
+  }
+
+  public SectionServlet(ApiUtil apiUtil) {
+    this.apiUtil = apiUtil;
+  }
+
+  /**
+   * Reads from Datastore and returns response with section details
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String courseId = request.getParameter("course_id");
+    if (courseId == null || courseId == "") {
+      respondWithError("Bad request.", HttpServletResponse.SC_BAD_REQUEST, response);
+      return;
+    }
+
+    URI uri;
+    try {
+      uri = new URI("https://api.umd.io/v1/courses/" + courseId + "/sections");
+    } catch (URISyntaxException e) {
+      respondWithError(
+          "Internal server error.", HttpServletResponse.SC_INTERNAL_SERVER_ERROR, response);
+      return;
+    }
+
+    JSONArray jsonArray = apiUtil.getJsonArray(uri);
+    if (jsonArray == null) {
+      respondWithError(
+          "Internal server error.", HttpServletResponse.SC_INTERNAL_SERVER_ERROR, response);
+      return;
+    }
+
+    JSONObject sectionsInfo = new JSONObject();
+    sectionsInfo.put("sections", jsonArray);
+
+    response.setContentType("application/json;");
+    response.getWriter().println(sectionsInfo);
+  }
+
+  private void respondWithError(String message, int errorType, HttpServletResponse response)
+      throws IOException {
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put("message", message);
+    jsonObject.put("status", "error");
+    response.setStatus(errorType);
+    response.getWriter().println(new Gson().toJson(jsonObject));
+  }
+}
