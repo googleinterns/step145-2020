ede02ab8ddc0fd8577118d98ee1671c8f8d2000b
ramyabuva <ramyasb@google.com>
2020-07-20 19:09:18 -0400

    Prettify files (#52)

--- src/main/webapp/js/courseSelector.js
+++ src/main/webapp/js/courseSelector.js
@@ -32,7 +32,9 @@ export const CollegePlanner = (() => {
       response = await fetch('/api/departments');
       departmentList = await response.json();
     } catch (err) {
-      createAlert('An error occurred', 'danger', document.getElementById('alert-container'));
+      createAlert(
+          'An error occurred', 'danger',
+          document.getElementById('alert-container'));
       return;
     }
     const departmentContainer = document.getElementById('departments');
@@ -78,7 +80,9 @@ export const CollegePlanner = (() => {
           `/api/courses?department=${encodeURIComponent(selectedDepartment)}`);
       courseList = await response.json();
     } catch (err) {
-      createAlert('An error occurred', 'danger', document.getElementById('alert-container'));
+      createAlert(
+          'An error occurred', 'danger',
+          document.getElementById('alert-container'));
       return;
     }
     const courseContainer = document.getElementById('courses');
@@ -155,20 +159,20 @@ export const CollegePlanner = (() => {
     return liElement;
   }
 
-/**
- * Creates an alert with the specified message in the container
- * @param {string} message The message string you want to be displayed
- * @param {string} type type of alert you want to display (primary, secondary,
- *     success, warning, danger)
- * @param {Element} container the container you want to display the alert in
- */
-function createAlert(message, type, container) {
-  const alert = document.createElement('div');
-  alert.setAttribute('class', `alert alert-${type}`);
-  alert.setAttribute('role', 'alert');
-  alert.appendChild(document.createTextNode(message));
-  container.appendChild(alert);
-}
+  /**
+   * Creates an alert with the specified message in the container
+   * @param {string} message The message string you want to be displayed
+   * @param {string} type type of alert you want to display (primary, secondary,
+   *     success, warning, danger)
+   * @param {Element} container the container you want to display the alert in
+   */
+  function createAlert(message, type, container) {
+    const alert = document.createElement('div');
+    alert.setAttribute('class', `alert alert-${type}`);
+    alert.setAttribute('role', 'alert');
+    alert.appendChild(document.createTextNode(message));
+    container.appendChild(alert);
+  }
 
   window.addEventListener('load', () => {
     getDepartmentOptions();
