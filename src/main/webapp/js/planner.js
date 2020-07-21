fe066d80e446084e795e57e745e66b4c24c36340
ramyabuva <ramyasb@google.com>
2020-07-21 12:58:32 -0400

    Add number of credits per semester to PlannerServlet (#49)
    * Change return type of getPlan() in PlannerServlet
    
    * Add semester_credits to PlannerServlet
    
    * Add credits per semester to front end
    
    * Remove unmodified files from PR
    
    * Add check for lengths of semester plan and semester credits

--- src/main/webapp/js/planner.js
+++ src/main/webapp/js/planner.js
@@ -41,12 +41,17 @@ async function getPlan() {
   }
   if (response.ok) {
     const courseData = courseList.semester_plan;
+    const creditsData = courseList.semester_credits;
     if (!courseData.length) {
       CollegePlanner.createAlert(
           'These courses did not fit in the given number of semesters.',
           'primary', courseContainer);
+    } else if (courseData.length == creditsData.length) {
+      createTable(courseData, creditsData, courseContainer);
     } else {
-      CollegePlanner.createTable(courseData, courseContainer);
+      CollegePlanner.createAlert(
+          'An invalid response was recieved.',
+          'warning', courseContainer);
     }
   } else {
     CollegePlanner.createAlert(courseList.message, 'warning', courseContainer);
@@ -67,9 +72,10 @@ function attachNewSpinner(courseContainer) {
 /**
  * Creates table from a 2D array
  * @param {Object} tableData 2D array with separation of courses
+ * @param {Object} creditsData Array with credits for each semester
  * @param {Element} courseContainer container for course list
  */
-function createTable(tableData, courseContainer) {
+function createTable(tableData, creditsData, courseContainer) {
   courseContainer.innerText = '';
   const table = document.createElement('table');
   const tableBody = document.createElement('tbody');
@@ -78,7 +84,7 @@ function createTable(tableData, courseContainer) {
     const row = document.createElement('tr');
     const cell = document.createElement('td');
     const semesterLabel = document.createElement('b');
-    semesterLabel.innerText = `Semester ${i + 1}:`;
+    semesterLabel.innerText = `Semester ${i + 1} (${creditsData[i]} Credits):`;
     cell.appendChild(semesterLabel);
     row.appendChild(cell);
 
