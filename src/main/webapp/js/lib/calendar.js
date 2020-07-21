5e3fc8aa0f169f76b047ef7c7ffd7760d884378a
savsa <savs@google.com>
2020-07-21 22:18:08 +0000

    Remove print statement.

--- src/main/webapp/js/lib/calendar.js
+++ src/main/webapp/js/lib/calendar.js
@@ -21,8 +21,6 @@ export const Calendar = (() => {
   return {addCourse: addCourse, initCalendar: initCalendar};
 })();
 
-console.log('bye');
-
 /**
  * The id of the next schedule that will be added to the calendar.
  * @type {number}
