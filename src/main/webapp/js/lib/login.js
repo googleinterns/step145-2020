417559d2fe4e27286d0c8822028293020985bccd
Ramya Bhaskara <ramyasb@google.com>
2020-07-31 21:45:42 +0000

    Add documentation to postSignInHandlers

--- src/main/webapp/js/lib/login.js
+++ src/main/webapp/js/lib/login.js
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/**
+ * Array of functions to execute after signing in in addition to onSignIn
+ * @type {!Array<function>}
+ */
 const postSignInHandlers = [];
 
 /**
